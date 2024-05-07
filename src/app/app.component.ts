281044777c1c05c27769e108c426e3a951c934f4
kailashgayari <kailash514910@gmail.com>
2024-05-07 11:39:50 +0530

    component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from "./components/hello-world/hello-world.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HelloWorldComponent]
 })
 export class AppComponent {
   title = 'my-angular-app';
